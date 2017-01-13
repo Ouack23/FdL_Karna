63390b77ff24da32477f6c55647f7f3e0598cac9
Ouack23 <ouack23@yahoo.fr>
2017-01-13 19:43:57 +0100

    Change pins.py

--- pins.py
+++ pins.py
@@ -3,7 +3,8 @@ import RPi.GPIO as GPIO
 
 
 def gpio_setup():
-    inputs = [2]
+    inputs = 2
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(True)
-    GPIO.setup(inputs, GPIO.INPUT)
+    GPIO.setup(inputs, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
