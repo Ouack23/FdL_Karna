cd980cae5ca19add09591f4f89dc5234228f9e3a
belette <ouack23@yahoo.fr>
2017-01-13 19:41:23 +0100

    Creation of shell file to launch program

--- pins.py
+++ pins.py
@@ -1,12 +1,9 @@
 import logging
-try:
-    import RPi.GPIO as GPIO
-except RuntimeError:
-    print("Error !")
+import RPi.GPIO as GPIO
 
 
 def gpio_setup():
     inputs = [2]
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(True)
-    GPIO.setup(inputs, GPIO.OUT)
+    GPIO.setup(inputs, GPIO.INPUT)
